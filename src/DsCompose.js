134271b5994ccf57dec8eed2dcf843bd254c8875
Larry Davis <lazdnet@gmail.com>
2021-01-12 12:49:07 -0800

    feat: add a top nav, which doesn't work

--- src/DsCompose.js
+++ src/DsCompose.js
@@ -1,9 +1,8 @@
 import { LitElement, html, css } from 'lit-element';
 
-import '@spectrum-web-components/theme/sp-theme';
-import '@spectrum-web-components/theme/theme-dark.js';
-import '@spectrum-web-components/theme/scale-medium.js';
 import '@spectrum-web-components/button/sp-button';
+import '@spectrum-web-components/top-nav/sp-top-nav';
+import '@spectrum-web-components/action-menu/sp-action-menu';
 
 export class DsCompose extends LitElement {
   static get properties() {
@@ -13,8 +12,15 @@ export class DsCompose extends LitElement {
   }
 
   static get styles() {
-    return css`
+    return css `
       :host {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        text-align: center;
+
+        background-color: var(--spectrum-alias-background-color-default);
       }
     `;
   }
@@ -25,10 +31,41 @@ export class DsCompose extends LitElement {
   }
 
   render() {
-    return html`
-      <sp-theme scale="medium" color="dark">
-        <sp-button>I'm supposed to be a button!</sp-button>
-      </sp-theme>
+    return html `
+      <sp-top-nav>
+        <sp-top-nav-item href="#">Compose</sp-top-nav-item>
+        <sp-top-nav-item href="#page-1" style="margin-inline-start: auto;">
+        System
+        </sp-top-nav-item>
+        <sp-top-nav-item href="#page-2">Components</sp-top-nav-item>
+        <sp-top-nav-item href="#page-3">Publish</sp-top-nav-item>
+        <sp-action-menu
+            label="Account"
+            placement="bottom-end"
+            style="margin-inline-start: auto;"
+        >
+            <sp-menu>
+                <sp-menu-item>
+                    Account Settings
+                </sp-menu-item>
+                <sp-menu-item>
+                    My Profile
+                </sp-menu-item>
+                <sp-menu-divider></sp-menu-divider>
+                <sp-menu-item>
+                    Share
+                </sp-menu-item>
+                <sp-menu-divider></sp-menu-divider>
+                <sp-menu-item>
+                    Help
+                </sp-menu-item>
+                <sp-menu-item>
+                    Sign Out
+                </sp-menu-item>
+            </sp-menu>
+        </sp-action-menu>
+      </sp-top-nav>
+      <sp-button>I'm a button!</sp-button>
     `;
   }
 }
--- src/ds-compose.js
+++ src/ds-compose.js
@@ -1,3 +1,16 @@
+// Base theme
+import '@spectrum-web-components/theme/sp-theme';
+import '@spectrum-web-components/theme/theme-dark.js';
+import '@spectrum-web-components/theme/scale-medium.js';
+
 import { DsCompose } from './DsCompose.js';
 
 customElements.define('ds-compose', DsCompose);
+
+// While https://github.com/open-wc/open-wc/issues/1210 and
+// go https://github.com/popperjs/popper-core/issues/933 persist
+// without an acceptable outcome, this allows the built storybook
+// to function with `process.env.NODE_ENV`... :/
+window.process = window.process || {};
+window.process.env = window.process.env || {};
+window.process.env.NODE_ENV = window.process.env.NODE_ENV || 'production';
